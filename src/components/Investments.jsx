91eb7c235c1a2d11056bda317f22e59dbe8fd143
ClyveGassant <cgassant@sas.upenn.edu>
2022-12-03 13:53:48 -0500

    partnerships

--- src/components/Investments.jsx
+++ src/components/Investments.jsx
@@ -1,5 +1,6 @@
 // imgs
 import involveButton from '../imgs/involvebutton.svg'
+import purplesquare from '../imgs/purplesquare.svg'
 
 // components
 import Navbar from './Navbar'
@@ -16,10 +17,10 @@ const Investments = () => {
         <div className="flex flex-col mx-32">
           {/* Welcome Text */}
           <div className="mt-32 text-6xl font-semibold w-3/4">
-          <span >
+            <span >
               Welcome to the
             </span>
-            <span className="ml-9 mr-9 text-purple"> 
+            <span className="ml-9 mr-9 text-purple">
               Investments
             </span>
             <span >
@@ -27,6 +28,47 @@ const Investments = () => {
             </span>
           </div>
         </div>
+
+        {/*Partnerships*/}
+        <div className="flex flex-col mx-44">
+          <div className="mt-44 text-4xl font-bold w-1/2">
+            <span className="text-white">
+              Partnerships
+            </span>
+          </div>
+
+        </div>
+
+        <div className="flex flex-col mx-32">
+          <div className="tracking-tight leading-5 text-base mt-8 text-[18px] w-1/2">
+            Our partners who make our projects
+          </div>
+
+          <div className="flex flex-col mx-32">
+            <div className="tracking-tight leading-5 text-base mt-8 text-[18px] w-1/2">
+              possible
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col mx-24">
+          <div className="tracking-tight leading-5 text-base mt-8 text-[18px] w-1/2">
+            Click here to learn more about our potential
+          </div>
+
+          <div className="flex flex-col mx-36">
+            <div className="tracking-tight leading-5 text-base mt-8 text-[18px] w-1/2">
+              partnerships
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <img src={purplesquare} alt="">
+          </img>
+        </div>
+
+
       </div>
     </>
   )
