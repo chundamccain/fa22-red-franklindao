d03be9df1e36e9470890505a122ecebbf184389c
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-12-10 16:30:32 -0500

    fixes research page

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -1,7 +1,7 @@
 // imgs
 import involveButton from '../imgs/involvebutton.svg'
 import twitter from '../imgs/twitter.png'
-
+import researchicon from '../imgs/research.png'
 
 // components
 import Navbar from './Navbar'
@@ -27,67 +27,52 @@ const Research = () => {
             <Person />
             <Person />
           </div>
-          {/* Welcome Text */}
-          {/*<div className="mt-32 text-6xl font-semibold w-3/4">
-            <span >
-              Welcome to the
-            </span>
-            <span className="ml-9 mr-9 text-purple">
-              Research
-            </span>
-            <span >
-              page
-            </span>
-  </div>*/}
         </div>
 
-        <div className="pt-60 pb-20">
-          <span className="flex flex-col justify-center items-center text-4xl font-semibold p-5">
+        <div className="pt-24 pb-20 flex flex-col justify-center items-center">
+          <span className="text-4xl font-semibold p-5">
             Spotlights
           </span>
-          <span className="flex flex-col justify-center items-center">
+          <span className="text-lg">
             Articles and past work done by operations
           </span>
-          <span className="flex flex-col justify-center items-center">
+          <span className="text-lg">
             commitee members
           </span>
         </div>
 
-        <div>
-          <div className='flex flex-row pl-10'>
-            <span className="flex flex-col justify-left text mx-32 text-4xl font-semibold pt-10">
-              Twitter
-            </span>
-            <div className='flex flex-row pr-10 pl-40'>
-              <div className='pr-10'>
-                <img className="w-36 h-24" src={twitter} alt="" />
-                <img className="w-36 h-24" src={twitter} alt="" />
-                <img className="w-36 h-24" src={twitter} alt="" />
-              </div>
-
-
-              <div className='flex flex-col pr-20 pt-5'>
-
-                <span className='pb-5'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                </span>
-
-                <span className='pb-5'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                </span>
-
-                <span className='pb-5'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                </span>
-              </div>
+        <div className='flex flex-row pl-10'>
+          <span className="flex flex-col justify-left text mx-32 text-4xl font-semibold pt-10">
+            Twitter
+          </span>
+          <div className='flex flex-row pr-10 pl-40'>
+            <div className='pr-10'>
+              <img className="w-36 h-30" src={twitter} alt="" />
+              <img className="w-36 h-30" src={twitter} alt="" />
+              <img className="w-36 h-30" src={twitter} alt="" />
             </div>
+
+
+            <div className='flex flex-col pr-20 pt-5'>
+              <span className='pb-5'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              </span>
+
+              <span className='pb-5 mt-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              </span>
+
+              <span className='pb-5 mt-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              </span>
+            </div>
+
           </div>
         </div>
 
         <div>
-          <span className="flex flex-col justify-center items-center text-4xl font-semibold pt-60">
+          <span className="flex flex-col justify-center items-center text-4xl font-semibold mt-40">
             Videos, Podcasts, Audio
-
             <div className='flex flex-row'>
             <MediaBlock name = {"TITLE"}/>
             <MediaBlock name = {"TITLE"}/>
@@ -100,36 +85,35 @@ const Research = () => {
         </div>
         {/*Videos*/}
           
-        <div>
+        <div className='flex flex-row pl-10'>
+          <span className="flex flex-col justify-left text mx-32 text-4xl font-semibold pt-10">
+            Long-form Research
+          </span>
+          <div className='flex flex-row pr-10'>
+            <div className='pr-10'>
+              <img className="w-32 h-26 py-6" src={researchicon} alt="" />
+              <img className="w-32 h-26 py-6" src={researchicon} alt="" />
+              <img className="w-32 h-26 py-6" src={researchicon} alt="" />
+            </div>
 
-        </div>
 
-        <div>
-          <div className='flex flex-row pl-10'>
-            <span className="flex flex-col justify-left text mx-32 text-4xl font-semibold pt-10">
-              Long-form Research
-            </span>
-
-
-
-            <div className='flex flex-col pr-20 pt-5 pl-48'>
-
+            <div className='flex flex-col pr-20 pt-5'>
               <span className='pb-5'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
               </span>
 
-              <span className='pb-5'>
+              <span className='pb-5 mt-24'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
               </span>
 
-              <span className='pb-5'>
+              <span className='pb-5 mt-20'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
               </span>
             </div>
+
           </div>
         </div>
       </div>
-
     </>
   )
 }
