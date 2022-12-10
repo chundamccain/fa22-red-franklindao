3f4a7e6ea99d74bb064c88f81bb26c72b6ab250c
ClyveGassant <cgassant@sas.upenn.edu>
2022-12-10 13:20:11 -0500

    commitee overview

--- src/components/Investments.jsx
+++ src/components/Investments.jsx
@@ -21,15 +21,15 @@ const Investments = () => {
         {/* Welcome section */}
         <div className="flex flex-col mx-32 items-center">
           {/* Welcome Text */}
-          <BigText name ={"Investments"} description ={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vol"}/>
-          
+          <BigText name={"Investments"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vol"} />
+
           <div className='flex flex-row '>
             <Person />
             <Person />
             <Person />
           </div>
 
-          <div className="mt-32 text-6xl font-semibold w-3/4">
+          {/*<div className="mt-32 text-6xl font-semibold w-3/4">
             <span >
               Welcome to the
             </span>
@@ -39,9 +39,9 @@ const Investments = () => {
             <span >
               page
             </span>
-          </div>
+          </div>*/}
         </div>
-        
+
         <div className="flex flex-row mt-40">
           {/*Partnerships*/}
           <div className="flex flex-col items-center basis-1/2">
@@ -57,21 +57,22 @@ const Investments = () => {
             </div>
 
             <div className="mt-6 tracking-tight leading-5 text-[18px]">
-              Click here to learn more about our potential 
+              Click here to learn more about our potential
             </div>
             <div className="tracking-tight leading-5 w-50 text-[18px]">
               partnerships
             </div>
           </div>
-          
+
           <div className="mr-20 grid grid-cols-3 basis-1/2">
             <div className="col-span-3">
               <Company company={"Company1"} />
             </div>
-            
+
             <Company company={"Company2"} />
             <Company company={"Company3"} />
             <Company company={"Company4"} />
+
 
             <div className="col-span-3">
               <Company className="col-span-3" company={"Company5"} />
@@ -88,9 +89,9 @@ const Investments = () => {
             <input className="w-[45rem] shadow border rounded-lg py-5 px-3 mt-14 text-center text-black text-lg leading-tight focus:outline-none focus:shadow-outline focus:border-purple " type="text" placeholder="Email" />
             <input className="w-[45rem] shadow border rounded-lg py-5 px-3 mt-10 text-center text-black text-lg leading-tight focus:outline-none focus:shadow-outline focus:border-purple " type="text" placeholder="Name" />
             <input className="w-[45rem] shadow border rounded-lg py-5 px-3 mt-10 text-center text-black text-lg leading-tight focus:outline-none focus:shadow-outline focus:border-purple " type="text" placeholder="Question" />
-          
+
             <button className="w-52 mt-10 mb-14">
-              <img src={submitButton} alt="submit button" className="hover:shadow-md"/>
+              <img src={submitButton} alt="submit button" className="hover:shadow-md" />
             </button>
           </div>
         </div>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,13 @@
 // imgs
 import graphics from '../imgs/graphics.svg'
 import involveButton from '../imgs/involvebutton.svg'
+import overview1 from '../imgs/overview1.svg'
+import overview2 from '../imgs/overview2.svg'
+import overview3 from '../imgs/overview3.svg'
+import overview4 from '../imgs/overview4.svg'
+import overview5 from '../imgs/overview5.svg'
+import overview6 from '../imgs/overview6.svg'
+import overview7 from '../imgs/overview7.svg'
 
 // components
 import Navbar from './Navbar'
@@ -18,7 +25,7 @@ const Home = () => {
 
         {/* Graphics */}
         <div className="absolute right-0 top-24">
-          <img src={graphics} className="h-screen" alt=""  />
+          <img src={graphics} className="h-screen" alt="" />
         </div>
 
 
@@ -29,7 +36,7 @@ const Home = () => {
             <span >
               Welcome to
             </span>
-            <span className="ml-9 text-purple"> 
+            <span className="ml-9 text-purple">
               FranklinDAO
             </span>
           </div>
@@ -41,12 +48,12 @@ const Home = () => {
 
           {/* Get Involved Button */}
           <button className="w-52">
-            <img src={involveButton} alt="involve button" className="hover:shadow-md"/>
+            <img src={involveButton} alt="involve button" className="hover:shadow-md" />
           </button>
 
           {/* Interest text */}
           <div className="mt-44 text-4xl font-bold w-1/2">
-            <span className="text-purple"> 
+            <span className="text-purple">
               Join
             </span>
             <span className="ml-6">
@@ -55,7 +62,7 @@ const Home = () => {
           </div>
 
           <div className="tracking-tight leading-5 text-base mt-8 text-[18px] w-1/2">
-            FranklinDAO draws from broad networks of alumni founders and investors, seeking continued collaboration with leading innovators and drivers in the space. 
+            FranklinDAO draws from broad networks of alumni founders and investors, seeking continued collaboration with leading innovators and drivers in the space.
           </div>
         </div>
 
@@ -65,8 +72,32 @@ const Home = () => {
         </div>
 
         {/* Commitee Overviews Section */}
-        <div>
-          
+        <div className='flex flex-col justify-center items-center pb-40'>
+
+          <span className='text-4xl font-semibold pt-60 pb-40'>
+            Commitee Overviews
+          </span>
+
+          <div className='flex flex-row pb-36'>
+            <img src={overview1} alt="" className="pr-40">
+            </img>
+            <img src={overview2} alt="" className="pr-20 pl-8">
+            </img>
+            <img src={overview3} alt="" className="pl-20 pr-8">
+            </img>
+            <img src={overview4} alt="" className="pl-40">
+            </img>
+          </div>
+
+          <div className='flex flex-row'>
+            <img src={overview5} alt="" className="pr-24">
+            </img>
+            <img src={overview6} alt="" className="pr-20 pl-20">
+            </img>
+            <img src={overview7} alt="" className="pl-24">
+            </img>
+          </div>
+
         </div>
       </div>
     </>
