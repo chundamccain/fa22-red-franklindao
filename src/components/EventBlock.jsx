7cdef1739d8109f16dc33fb4a33759f15276b3a8
exyzhao <ethanxyzhao@gmail.com>
2023-01-08 22:40:58 -0600

    Integrate backend for operations

--- src/components/EventBlock.jsx
+++ src/components/EventBlock.jsx
@@ -1,7 +1,7 @@
 import { useMediaQuery } from 'react-responsive'
 
 
-const EventBlock = ({title}) => {
+const EventBlock = ({title, image}) => {
 
   const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 800px)'})
   const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
@@ -18,7 +18,7 @@ const EventBlock = ({title}) => {
         </div>
 
         <div className="flex flex-col w-72 py-2 px-2 rounded-lg bg-dark_purple">
-          <img className = "border-blue rounded-lg" src="https://d.newsweek.com/en/full/1905075/file-photo-smiling-dog.jpg" alt = ""/>
+          <img className = "border-blue rounded-lg" src={image} alt = ""/>
           <button className="mt-4 text-lg">
             read more
           </button> 
@@ -33,7 +33,7 @@ const EventBlock = ({title}) => {
         </div>
 
         <div className="flex flex-col w-72 py-2 px-2 rounded-lg bg-dark_purple">
-          <img className = "border-blue rounded-lg" src="https://d.newsweek.com/en/full/1905075/file-photo-smiling-dog.jpg" alt = ""/>
+          <img className = "border-blue rounded-lg" src={image} alt = ""/>
           <button className="mt-4 text-lg">
             read more
           </button> 
--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react'
+import db from "../lib/firebase"
+
 // imgs
 import involveButton from '../imgs/involvebutton.svg'
 import Person from './AdaptPerson'
@@ -18,73 +21,87 @@ import Sofia from "../imgs/SofiaWawrzyniak_UndergraduatePresident.jpg"
 
 const Development = () => {
 
-
   const portraits = [
-    {image: Sofia, name: "Sofia Wawrzyniak", description: "Undergraduate President", bio: "Insert Small Bio"},
-    { image: Omar, name :"Omar Ameen", description : "Events Director", bio : "Insert Small Bio"},
-    {image: Kirill, name : "Kirill Naumov", description : "Investments Director", bio :"Insert Small Bio"},
-   
+    { image: Sofia, name: "Sofia Wawrzyniak", description: "Undergraduate President", bio: "Insert Small Bio" },
+    { image: Omar, name: "Omar Ameen", description: "Events Director", bio: "Insert Small Bio" },
+    { image: Kirill, name: "Kirill Naumov", description: "Investments Director", bio: "Insert Small Bio" },
+
   ]
 
-
-  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 800px)'})
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 800px)' })
   const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 800px)' })
   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
   const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
-  
+  const [spotlights, setSpotlights] = useState([]);
+
+  // Retrieve firestore data
+  useEffect(() => {
+    db.collection("operations-spotlights")
+      .orderBy("order")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+
+        setSpotlights(data)
+      })
+  }, [])
+
 
   return (
     <>
-    {isTabletOrMobile && 
-      <div className="flex flex-col h-screen text-white font-mono bg-background ">
-        <div className="py-12 ">
-          <NavDrop />
-        </div>
-
-        {/* Welcome section */}
-        <div className="flex flex-col mx-32 items-center">
-          <BigText name={"Operations"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vol"} size={"940px"} />
-         
-          <Person image = {portraits[0].image} name = {portraits[0].name} description = {portraits[0].description} bio = {portraits[0].bio}/>
-          <Person image = {portraits[1].image} name = {portraits[1].name} description = {portraits[1].description} bio = {portraits[1].bio}/>
-          <Person image = {portraits[2].image} name = {portraits[2].name} description = {portraits[2].description} bio = {portraits[2].bio}/>
-         
-          <BigText name={"Spotlights"} description={"Articles and past work done by operations committee members"} size={"100px"} />
-
-          <Events title={"TITLE"} />
-            <Events title={"TITLE"} />
-            <Events title={"TITLE"} />
-        </div>
-      </div>
-}
-
-    {isDesktopOrLaptop && 
-      <div className="flex flex-col h-screen text-white font-mono bg-background ">
-        <div className="py-12 ">
-          <Navbar />
-        </div>
-
-        {/* Welcome section */}
-        <div className="flex flex-col mx-32 items-center">
-          <BigText name={"Operations"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vol"} size={"940px"} />
-          <div className='flex flex-row '>
-          <div className = "mx-5"> <Person image = {portraits[0].image} name = {portraits[0].name} description = {portraits[0].description} bio = {portraits[0].bio}/></div>
-          <div className = "mx-5"><Person image = {portraits[1].image} name = {portraits[1].name} description = {portraits[1].description} bio = {portraits[1].bio}/></div>
-          <div className = "mx-5"><Person image = {portraits[2].image} name = {portraits[2].name} description = {portraits[2].description} bio = {portraits[2].bio}/></div>
-          </div>
-          <BigText name={"Spotlights"} description={"Articles and past work done by operations committee members"} size={"100px"} />
-
-          <div className='flex flex-row pb-60' >
-            <Events title={"TITLE"} />
-            <Events title={"TITLE"} />
-            <Events title={"TITLE"} />
+      {isTabletOrMobile &&
+        <div className="flex flex-col h-screen text-white font-mono bg-background ">
+          <div className="py-12 ">
+            <NavDrop />
           </div>
 
+          {/* Welcome section */}
+          <div className="flex flex-col mx-32 items-center">
+            <BigText name={"Operations"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vol"} size={"940px"} />
+
+            <Person image={portraits[0].image} name={portraits[0].name} description={portraits[0].description} bio={portraits[0].bio} />
+            <Person image={portraits[1].image} name={portraits[1].name} description={portraits[1].description} bio={portraits[1].bio} />
+            <Person image={portraits[2].image} name={portraits[2].name} description={portraits[2].description} bio={portraits[2].bio} />
+
+            <BigText name={"Spotlights"} description={"Articles and past work done by operations committee members"} size={"100px"} />
+
+            {spotlights.map(spotlight => (
+              <Events title={spotlight.title} />
+            ))}
+          </div>
         </div>
-      </div>
-}
+      }
+
+      {isDesktopOrLaptop &&
+        <div className="flex flex-col h-screen text-white font-mono bg-background ">
+          <div className="py-12 ">
+            <Navbar />
+          </div>
+
+          {/* Welcome section */}
+          <div className="flex flex-col mx-32 items-center">
+            <BigText name={"Operations"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vol"} size={"940px"} />
+            <div className='flex flex-row '>
+              <div className="mx-5"> <Person image={portraits[0].image} name={portraits[0].name} description={portraits[0].description} bio={portraits[0].bio} /></div>
+              <div className="mx-5"><Person image={portraits[1].image} name={portraits[1].name} description={portraits[1].description} bio={portraits[1].bio} /></div>
+              <div className="mx-5"><Person image={portraits[2].image} name={portraits[2].name} description={portraits[2].description} bio={portraits[2].bio} /></div>
+            </div>
+            <BigText name={"Spotlights"} description={"Articles and past work done by operations committee members"} size={"100px"} />
+
+            <div className='flex flex-row pb-60' >
+              {spotlights.map(spotlight => (
+                <Events title={spotlight.title} image={spotlight.image} />
+              ))}
+            </div>
+
+          </div>
+        </div>
+      }
     </>
   )
 }
