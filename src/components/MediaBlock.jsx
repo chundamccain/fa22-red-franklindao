2c852850de655eaec95d7a5419169ecc9a6cd019
paras-nahar <pnahar@seas.upenn.edu>
2023-05-13 10:20:15 +0100

    video

--- src/components/MediaBlock.jsx
+++ src/components/MediaBlock.jsx
@@ -5,7 +5,8 @@ const MediaBlock = ({name, image, link}) => {
       <div className="flex flex-col items-center mt-12 mb-20 ml-3 mr-3">
         <div className="flex flex-col w-72 py-6 px-6 rounded-3xl border-4 border-purple">
           <div className = "object-cover w-80 h-60 border-blue rounded-lg" alt = "">
-            <a href={link}><img src={image} alt="PBC Shorts" border="0"/></a>
+            {/* <a href={link}><img src={image} alt="PBC Shorts" border="0"/></a> */}
+            <iframe width="100%" height="auto" src={link} title="Is centralization inevitable? 🤔" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
           <button className="mt-8 py-2 text-lg border-4 border-purple text-purple underline rounded-full">
             {name}
