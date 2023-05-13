02c912e5a697157abb2cd11a7e7e1e985b3c146d
paras-nahar <pnahar@seas.upenn.edu>
2023-05-13 10:38:28 +0100

    sizing2

--- src/components/MediaBlock.jsx
+++ src/components/MediaBlock.jsx
@@ -6,7 +6,7 @@ const MediaBlock = ({name, image, link}) => {
         <div className="flex flex-col w-72 py-6 px-6 rounded-3xl border-4 border-purple">
           <div className = "object-cover w-80 h-60 border-blue rounded-lg" alt = "">
             {/* <a href={link}><img src={image} alt="PBC Shorts" border="0"/></a> */}
-            <iframe className="object-cover w-80 h-60 border-blue rounded-lg" src="https://www.youtube.com/embed/tKuN01si_0I" title="Is centralization inevitable? 🤔" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe className="object-cover w-80 h-60 border-blue rounded-lg" width="80%" src="https://www.youtube.com/embed/tKuN01si_0I" title="Is centralization inevitable? 🤔" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
           <button className="mt-8 py-2 text-lg border-4 border-purple text-purple underline rounded-full">
             {name}
